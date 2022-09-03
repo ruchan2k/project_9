926713ec31f194b3d6f19a57e1e1b34c2bac9a14
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-03 08:33:53 +0300

    Initial Commit

---
+++ project_9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project_9
+//
+//  Created by Ruçhan Talha Günay on 3.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
